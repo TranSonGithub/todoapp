18ec44590d442b5311a832b27a3056d4b4a75b87
GitTranSon <tranvanson1008@gmail.com>
2021-04-02 22:42:29 +0700

    them chuc nang sua xoa va thay doi giao dien

--- router/todo.js
+++ router/todo.js
@@ -9,7 +9,7 @@ router.get('/',async (req, res) => {
     let todoList = await ToDo.find({})
     res.render('todo', {todoList: todoList})
   } catch {
-    res.redirect('/todos')
+    res.redirect('/')
   }
     
   
@@ -20,13 +20,48 @@ router.post('/', async (req, res) => {
     content: req.body.content
   })
   try {
-    let newTodo = await todo.save()
-    res.redirect('/todos')
+    await todo.save()
+    res.redirect('/')
   } catch(e) {
     res.render('todo', {todoList: todo, error: e})
   }
-  
+})
 
+// Edit todo 
+router.get('/:id', async (req, res) => {
+  try {
+    let todo = await ToDo.findById(req.params.id)
+    res.render('todoEdit', {todo: todo})
+    
+  } catch {
+    res.redirect('/')
+  }
+})
+router.put('/:id', async (req, res) => {
+  let newTodo
+  try {
+    newTodo = await ToDo.findById(req.params.id)
+    newTodo.content = req.body.newContent
+    
+    await newTodo.save()
+    res.redirect('/')
+  } catch {
+    if (newTodo == null) {
+      console.log('null')
+      res.redirect('/')
+    } else {
+      res.render('todoEdit', {todo: newTodo, error: 'Error updating'})
+    }
+  }
+})
+router.delete('/:id', async (req, res) => {
+  try {
+    delTodo = await ToDo.findById(req.params.id)
+    await delTodo.remove()
+    res.redirect('/')
+  } catch {
+    res.redirect('/')
+  }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 
 require('dotenv').config()
 const PORT = process.env.PORT || 3000
@@ -13,6 +14,7 @@ app.set('view engine', 'ejs')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(methodOverride('_method'))
 
 // transform data to json
 app.use(express.json())
@@ -25,11 +27,9 @@ db.once('open', () => console.log('Connect database success'))
 db.on('error', (e) => console.log(e))
 
 // Router app
-const index = require('./router/index')
 const todo = require('./router/todo')
 
-app.use('/', index)
-app.use('/todos', todo)
+app.use('/', todo)
 
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
